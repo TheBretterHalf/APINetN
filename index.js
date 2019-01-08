18786214b7f72934466f2bcf37465886587255df
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-08 00:54:00 +0000

    app.get('/api', function(req, res)

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/api', function(req, res){
+    console.log('GET request')
+    res.send({name:'yoshi'});
+});
+
 //listen for requests
-app.listen(process.env.port || 4000, function(){
+app.listen(process.env.PORT, process.env.IP , function(){
     console.log('Now Listening for requests');
 });
