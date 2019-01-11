7581f98204bb364c98919f2060a6a00cb659dbf8
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-11 00:10:27 +0000

    app.use('/api', require('./routes/api'));

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+//get list of ninjas from db
+router.get('/ninjas', function (req, res){
+    res.send({type: 'GET'});    
+});
+//add new ninja to db
+router.post('/ninjas', function (req, res){
+    res.send({type: 'POST'})    
+});
+//udpate ninja in db
+router.put('/ninjas/:id', function (req, res){
+    res.send({type: 'PUT'})    
+});
+//delete ninja from db
+router.delete('/ninjas/:id', function (req, res){
+    res.send({type: 'DELETE'})    
+});
+
+module.exports = router;
