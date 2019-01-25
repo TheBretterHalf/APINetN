6a4c6f795ccc1e72fccaf3990b61893c3562eb21
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-25 01:42:27 +0000

    NInja.Create

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const bodyParser = require('body-parser');
 
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 // set up express app
 const app = express();
+
+//connect to mongoDB
+mongoose.connect('mongodb://localhost/ninjago', {useNewUrlParser: true});
+
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json())
 
