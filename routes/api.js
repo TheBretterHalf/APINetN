1f2896a314467d6544e372fed7e1293198c49899
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-28 04:15:30 +0000

    findOne

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,11 @@ router.post('/ninjas', function (req, res, next){
 });
 //udpate ninja in db
 router.put('/ninjas/:id', function (req, res, next){
-    res.send({type: 'PUT'})    
+    Ninja.findByIdAndUpdate({_id:req.params.id}.req.body).then(function(){
+        Ninja.findOne({_id: req.params.id}).then(function(ninja){
+            res.send({ninja})
+        });
+    });
 });
 //delete ninja from db
 router.delete('/ninjas/:id', function (req, res, next){
