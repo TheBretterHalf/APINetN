18293d29543166696bdf9796004a41ba2ad0b970
Brett Stainken <brettstainken.student@careerdevs.com>
2019-01-28 04:35:23 +0000

    Geometry, GeoJson

--- Models/ninja.js
+++ Models/ninja.js
@@ -1,6 +1,18 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
+
+//create geolocation schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    }
+});
 //create ninja Schema and model
 const NinjaSchema = new Schema({
     name: {
@@ -13,10 +25,24 @@ const NinjaSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
-    //add in geo location
+    },
+    geometry: GeoSchema
+    
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
 
 module.exports = Ninja;
+
+
+// {
+//   "type": "Feature",
+//   "geometry": {
+//     "type": "Point",
+//     "coordinates": [125.6, 10.1]
+//   },
+//   "properties": {
+//     "name": "Dinagat Islands"
+//   }
+// }
+
